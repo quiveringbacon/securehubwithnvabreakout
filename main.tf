7ad37b2f7effd721cacc28d1ac24b4b1abd52e53
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2024-08-28 09:38:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 provider "azurerm" {
   features {
   }
+subscription_id = var.F-SubscriptionID
 }
 
 #variables
@@ -25,6 +26,10 @@ variable "D-username" {
 variable "E-password" {
     description = "Password for Virtual Machines"
     sensitive = true
+}
+
+variable "F-SubscriptionID" {
+  description = "Subscription ID to use"  
 }
 
 resource "azurerm_resource_group" "RG" {
